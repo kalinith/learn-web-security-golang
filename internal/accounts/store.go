a2428273d44fc000b218f43df85f7634d68aaaa4
Neil van Vuuren <kalinith@gmail.com>
2026-09-18 22:32:40 +0200

    Ch2.1 Authentication normalize email input by trimming whitespace and converting to lowercase

--- internal/accounts/store.go
+++ internal/accounts/store.go
@@ -10,6 +10,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strings"
 	"time"
 
 	"github.com/bootdotdev/learn-web-security/internal/database/dbgen"
@@ -20,7 +21,8 @@ const defaultSessionTTL = 30 * 24 * time.Hour
 var ErrEmailExists = errors.New("an account already exists for that email")
 
 func NormalizeEmail(email string) string {
-	return email
+	cleanedEmail := strings.ToLower(strings.TrimSpace(email))
+	return cleanedEmail
 }
 
 type User struct {
